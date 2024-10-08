e54afec9a545808092da1d7ac5710923ab705a6e
yashgulia <yashgulia09@gmail.com>
2024-10-08 18:02:53 +0530

    Update UI with Fetched Data

--- index.js
+++ index.js
@@ -85,3 +85,26 @@ async function fetchWeatherInfo(coordinates) {
     errorContainer.classList.add("active");
   }
 }
+
+// Show fetched data from API on UI
+function renderWeatherInfo(weatherInfo) {
+  // Fetch elements
+  const cityName = document.querySelector("[data-cityName]");
+  const countryIcon = document.querySelector("[data-countryIcon]");
+  const desc = document.querySelector("[data-weatherDesc]");
+  const weatherIcon = document.querySelector("[data-weatherIcon]");
+  const temp = document.querySelector("[data-temp]");
+  const windspeed = document.querySelector("[data-windspeed]");
+  const humidity = document.querySelector("[data-humidity]");
+  const cloudiness = document.querySelector("[data-cloudiness]");
+
+  // Destructure weather data and put into UI
+  cityName.innerText = weatherInfo?.name;
+  countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+  desc.innerText = weatherInfo?.weather?.[0]?.description;
+  weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+  temp.innerText = `${weatherInfo?.main?.temp} °C`;
+  windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+  humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+  cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
+}
