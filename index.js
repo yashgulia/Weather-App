a151d2f727e622ae41bef06dd6ba7d47b9688ee6
yashgulia <yashgulia09@gmail.com>
2024-10-08 17:12:44 +0530

    Search Weather Tab UI during Switching

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const userTab = document.querySelector("[data-userWeather]");
 const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-container");
+const grantAccessContainer = document.querySelector(".grant-location");
+const searchContainer = document.querySelector(".form-container");
+const loader = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".info-container");
 
 let currentTab = userTab;
 currentTab.classList.add("current-tab");
 
 function switchTab(switchedTab) {
+  // If we are not clicking on same(current) Tab
   if (switchedTab != currentTab) {
     currentTab.classList.remove("current-tab");
     currentTab = switchedTab;
     currentTab.classList.add("current-tab");
+
+    // If we are now present in Search Weather Tab after switching
+    if (!searchContainer.classList.contains("active")) {
+      userInfoContainer.classList.remove("active");
+      grantAccessContainer.classList.remove("active");
+      searchContainer.classList.add("active");
+    }
+    // If we are now present in Your Weather Tab after switching
+    else {
+      searchContainer.classList.remove("active");
+      userInfoContainer.classList.remove("active");
+      getfromSessionStorage();
+    }
   }
 }
 
--- readme.txt
+++ readme.txt
@@ -2,3 +2,5 @@ Creating Weather App using HTML, CSS Preprocessor and JS.
 
 - Basic HTML Structure of our App
 - Create CSS for All 4 UI's to display accordingly
+- switchTab() -> It will switch the tab accordingly and change content of tab as well.
+- sessionStorage() -> It will find if coordinates are stored in storage or not based on which, UI have to be shown for Your Weather tab.
