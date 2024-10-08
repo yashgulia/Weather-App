0096db925ad4cffe063330bcf66f5cc57ae5c7ab
yashgulia <yashgulia09@gmail.com>
2024-10-08 15:54:03 +0530

    Highlighting Current Tab Functionality

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
 
+let currentTab = userTab;
+currentTab.classList.add("current-tab");
+
+function switchTab(switchedTab) {
+  if (switchedTab != currentTab) {
+    currentTab.classList.remove("current-tab");
+    currentTab = switchedTab;
+    currentTab.classList.add("current-tab");
+  }
+}
+
+userTab.addEventListener("click", () => {
+  switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+  switchTab(searchTab);
+});
